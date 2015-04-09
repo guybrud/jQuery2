f83ed2207748b58dcb074ddfa30929f485e8b0e2
guybrud <guybrud@gmail.com>
2015-04-09 15:34:29 -0600

    final debugging

--- appstuff/app.js
+++ appstuff/app.js
@@ -1,4 +1,31 @@
-$(document).ready(function( {
+$(document).ready(function( ) {
+
+	
+
+
+	var save = function() {
+	localStorage.listo = JSON.stringify(listo);
+	};
+
+
+
+	var advanceTask = function(task) {
+	    var modified = task.innerText.trim()
+	    for (var i = 0; i < listo.length; i++) {
+	        if (listo[i].task === modified) {
+	            if (listo[i].id === 'new') {
+	                listo[i].id = 'inProgress';
+	            } else if (listo[i].id === 'inProgress') {
+	                listo[i].id = 'archived';
+	            } else {
+	                listo.splice(i, 1);
+	            }
+	            save();
+	            break;
+	        }
+	    }
+	    task.remove();
+	};
 
 	var list = [];
 	var Task = function(task) {
@@ -28,9 +55,9 @@ $(document).ready(function( {
 		$('#newTaskForm, #newListItem').fadeToggle('fast', 'linear');
 	});
 
-	$('#cancel').on('click', function (e) {
+	$('#cancel').on('click', function(e) {
 		e.preventDefaulf();
-		$('#newTaskForm, #newListItem').fadeToggle('fast'. 'linear');
+		$('#newTaskForm,  #newListItem').fadeToggle('fast', 'linear');
 	});
 
 	$(document).on('click', '#item', function(e) {
@@ -50,30 +77,6 @@ $(document).ready(function( {
 	});
 
 
-	var save = function() {
-	localStorage.listo = JSON.stringify(listo);
-	};
-
-
-
-	var advanceTask = function(task) {
-	    var modified = task.innerText.trim()
-	    for (var i = 0; i < listo.length; i++) {
-	        if (listo[i].task === modified) {
-	            if (listo[i].id === 'new') {
-	                listo[i].id = 'inProgress';
-	            } else if (listo[i].id === 'inProgress') {
-	                listo[i].id = 'archived';
-	            } else {
-	                listo.splice(i, 1);
-	            }
-	            save();
-	            break;
-	        }
-	    }
-	    task.remove();
-	};
-
 	$(document).on('click', '#archived', function(e) {
 		e.preventDefault();
 		var task = this;
@@ -83,4 +86,4 @@ $(document).ready(function( {
 
 
 
-}));
+});
